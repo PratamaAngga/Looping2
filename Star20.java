e02950f92ad4f02ea3b29044472997d6d1ec6a94
PratamaAngga <anggashillnoir@gmail.com>
2024-11-08 08:49:20 +0700

    after question experiment 1 and 2

--- Star20.java
+++ Star20.java
@@ -5,7 +5,7 @@ public class Star20 {
         System.out.print("Enter value N = ");
         int N = sc.nextInt();
 
-        for(int i=1; i<=N; i++) {
+        for(int i=1; i<=N; i--) {
             System.out.print("*");
         }
     }
